0a49ad9f5d3de3db681a5c2b6da1921fece4b725
Khan <khan4019@gmail.com>
2016-07-21 08:11:44 -0500

    slow progress

--- managerTraker/app/scripts/main.js
+++ managerTraker/app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+var views = document.getElementsByClassName('view');
+for (var i = 0; i < views.length; i++) {
+  views[i].addEventListener('click', function() {
+  	window.location.href = i +'.html';
+ });
+}
